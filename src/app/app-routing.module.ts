dfba112379b298a8993cfd440f0da6c087750219
abhi <abhibezavada@gmail.com>
2025-12-16 22:59:11 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { IphoneComponent } from './navigatingcomponents/iphone/iphone.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { AndroidComponent } from './navigatingcomponents/android/android.component';
+import { HelpComponent } from './navigatingcomponents/help/help.component';
+import { CompanyComponent } from './navigatingcomponents/company/company.component';
+import { SigninComponent } from './navigatingcomponents/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',component:WelcomeComponent},
+{path:'iphone',component:IphoneComponent},
+{path:'android',component:AndroidComponent},
+{path:'help',component:HelpComponent},
+{path:'company',component:CompanyComponent},
+{path:'signin',component:SigninComponent},
+{path:'**', component:PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+ 
+ }
